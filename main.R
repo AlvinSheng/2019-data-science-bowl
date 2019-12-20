615f87f1ca6da760c5ce67f021e0565d7571ab4d
AlvinSheng <as133@rice.edu>
2019-12-20 13:18:05 -0500

    Revising code so it'd work in a Kaggle kernel

--- main.R
+++ main.R
@@ -1,40 +1,38 @@
 
 library(rstudioapi)
-library(drake)
 library(tools)
 library(dplyr)
-
-source("wrangle.R")
 
 # This sets the working directory to that of this file main.R 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+
+# Reading in all the files
 data_folder <- "data-science-bowl-2019"
+
 data_files <- list_files_with_exts(data_folder, "csv")
 
+data_names <- data_files %>% basename() %>% file_path_sans_ext()
 
+dat <- lapply(data_files, read.csv)
 
-wrangle_plan <- drake_plan(
-  
-  dat = read_dat(data_files), 
-  train = dat$train,
-  test = dat$test
-  
-)
-
-plan <- rbind(wrangle_plan)
-
-config <- drake_config(plan)
-
-vis_drake_graph(config)
+names(dat) <- data_names
 
 
 
-make(plan)
+# Extracting the required datasets
+train <- dat$train
+test <- dat$test
 
-history <- drake_history(analyze = TRUE)
+sample_submission <- dat$sample_submission
+specs <- dat$specs
+train_labels <- dat$train_labels
 
-cache <- drake_cache()
+
+
+
+
+
 
 
 
